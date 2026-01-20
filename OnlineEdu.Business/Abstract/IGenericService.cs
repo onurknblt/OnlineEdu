259d853ce86294751c9a1573c64dd27715acc8d9
onurknblt <onurknblt@gmail.com>
2026-01-21 02:54:29 +0300

    Soyut(Abstract) ve Somut(Concrete) yapılar temel olarak oluşturuldu.

---
+++ OnlineEdu.Business/Abstract/IGenericService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineEdu.Business.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        T GetByFilter(Expression<Func<T, bool>> predicate);
+        int TFilteredCount(Expression<Func<T, bool>> predicate);
+        List<T> TGetFilteredList(Expression<Func<T, bool>> predicate);
+        List<T> TGetList();
+        T TGetById(int id);
+        void TCreate(T entity);
+        void TUpdate(T entity);
+        void TDelete(int id);
+        int TCount();
+        
+    }
+}
